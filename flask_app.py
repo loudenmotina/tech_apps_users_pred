407cbe814a9a032cad16bda0eef14137ae827e58
louden besingi motina <115381972+loudenmotina@users.noreply.github.com>
2023-10-27 08:31:16 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from pickle import dump, load
 app = Flask(__name__)
 
 # Load your trained RandomFo
-# You should replace 'your_model.pkl' with the actual filename of your trained model
+# You should replace 'your_model.pkl' with the actual filename of your trained modelkkklll
 model = joblib.load(open("model.pkl", "rb"))  # Load your trained model here
 
 @app.route('/')
